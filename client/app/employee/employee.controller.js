cba4ebc2eacfeecdd49af94854d69237465a3814
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2016-11-01 16:54:19 -0500

    errors fixed

--- client/app/employee/employee.controller.js
+++ client/app/employee/employee.controller.js
@@ -6,13 +6,9 @@
       var errors = {};
       var submitted = false;
 
-      function paginatorCallback(page, pageSize){
-                  var offset = (page-1) * pageSize;
-
-                  
       this.employees = Employee.query();
 //      console.log(Employee.getOne({},{_id:'5818ede4ec0c0893d3b9f429'}));
                         //          ^A   ^ B
-// A is the first param that is passed  and its compared to B
-    });
+
+    })
 }());
