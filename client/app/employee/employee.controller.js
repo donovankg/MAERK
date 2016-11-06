821036ca58351cdc9771e4fdbd46a57e263b0bbb
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2016-11-06 15:46:22 -0600

    made it so that icons change depending on how many boxes are checked

--- client/app/employee/employee.controller.js
+++ client/app/employee/employee.controller.js
@@ -3,7 +3,7 @@
 
 
   angular.module('maerkApp.employee')
-    .controller('EmployeeController', function(Empfactory, $mdToast, $mdDialog) {
+    .controller('EmployeeController', function(Empfactory, $mdToast, $mdDialog, $scope) {
       var errors = {};
       var submitted = false;
       var self = this;
@@ -35,6 +35,26 @@
     this.showEdit;
     this.selectedRowCallback = function(rows){
       console.log(rows);
+      if (rows.length ==0){
+        this.editBtn = false;
+        this.deleteBtn = false;
+        this.activeBtn = false;
+        this.addBtn = false;
+      }else if (rows.length== 1) {
+        this.addBtn = true;
+        this.editBtn = true;
+        this.deleteBtn = true;
+        this.activeBtn = true;
+
+        // addBtn = true;
+      }else {
+        this.addBtn = true;
+        this.editBtn = false;
+        this.deleteBtn = true;
+        this.activeBtn = true;
+
+
+      }
 
       this.showEdit=true;
 
